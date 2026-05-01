5d20954a2d5b8e59ef393e6098f773cd2acc1f41
daxprocoder <daxprocoder@gmail.com>
2026-05-01 22:37:57 +0530

    Global CORS fix for Vercel and Mobile

--- product_backend/server.js
+++ product_backend/server.js
@@ -24,9 +24,13 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
+
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
