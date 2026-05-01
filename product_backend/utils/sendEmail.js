ed66192a61a5b25eba6c9c2905c83c3ceac86570
Dax <145893566+daxprocoder@users.noreply.github.com>
2026-05-01 19:34:23 +0530

    Update sendEmail.js

--- product_backend/utils/sendEmail.js
+++ product_backend/utils/sendEmail.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for 587
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
